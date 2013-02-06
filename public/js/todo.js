e8b76ebcb43940863df7018ee203daf688025831
Roberto Torella <robertot@square-enix.com>
2013-02-06 17:25:40 +0000

    Client side ToDo Exercise completed

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,16 +1,5 @@
-angular.module('todo', []).
-	config(function($routeProvider) {
-		$routeProvider.
-			when('/', {controller:TodoCtrl, templateUrl:'list.html'}).
-			otherwise({redirectTo:'/'});
-	});
-
 function TodoCtrl($scope) {
 	$scope.todos = [];
-
-	$scope.getTotalTodos = function() {
-		return $scope.todos.length;
-	}
 
 	$scope.clearCompleted = function() {
 		$scope.todos = $scope.todos.filter(function(todo){
@@ -19,48 +8,10 @@ function TodoCtrl($scope) {
 	}
 
 	$scope.addTodo = function() {
-		$scope.todos.push({text:$scope.formTodoText, done:false});
-		$scope.totalTodos=$scope.todos.length;
-		$scope.formTodoText = "";
+		if ($scope.formTodoText) {
+			$scope.todos.push({text:$scope.formTodoText, done:false});
+			$scope.totalTodos=$scope.todos.length;
+			$scope.formTodoText = "";
+		}
 	}
 }
-
-
-/*function ListCtrl($scope, Project) {
-	$scope.projects = Project.query();
-}
-
-
-function CreateCtrl($scope, $location, Project) {
-	$scope.save = function() {
-		Project.save($scope.project, function(project) {
-			$location.path('/edit/' + project._id.$oid);
-		});
-	}
-}
-
-
-function EditCtrl($scope, $location, $routeParams, Project) {
-	var self = this;
-
-	Project.get({id: $routeParams.projectId}, function(project) {
-		self.original = project;
-		$scope.project = new Project(self.original);
-	});
-
-	$scope.isClean = function() {
-		return angular.equals(self.original, $scope.project);
-	}
-
-	$scope.destroy = function() {
-		self.original.destroy(function() {
-			$location.path('/list');
-		});
-	};
-
-	$scope.save = function() {
-		$scope.project.update(function() {
-			$location.path('/');
-		});
-	};
-}//*/
